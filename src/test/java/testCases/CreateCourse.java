18d15f01d6bada51331b3855ac410400c79b88c5
swapnilmane8727 <swapnilmane8727@gmail.com>
2024-11-05 22:34:26 +0530

    added a new TC for course creation

---
+++ src/test/java/testCases/CreateCourse.java
@@ -0,0 +1,21 @@
+package testCases;
+
+import org.testng.annotations.Test;
+
+import base.BaseClass;
+
+public class CreateCourse extends BaseClass
+{
+
+	
+	@Test
+	public void createCourse()
+	{
+		
+	}
+	
+	
+	
+	
+	
+}
